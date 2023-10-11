373dec7e4501038ea3f520bed2273622d3456a61
Adhe Rama F <adheramaf7@gmail.com>
2023-10-11 15:50:23 +0700

    setting public routes in middleware

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { BillboardColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
 
 interface BillboardClientProps {
    data: BillboardColumn[];
@@ -32,6 +33,9 @@ const BillboardClient = ({ data }: BillboardClientProps) => {
          </div>
          <Separator />
          <DataTable columns={columns} data={data} searchKey="label" />
+         <Heading title="API" description="API calls for Billboard" />
+         <Separator />
+         <ApiList entityName="billboards" entityIdName="billboardId" />
       </>
    );
 };
