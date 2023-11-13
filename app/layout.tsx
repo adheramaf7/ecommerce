09a2c3da4fb0b5980e22167562d5d3cc38a038bf
Adhe Rama F <adheramaf7@gmail.com>
2023-11-13 15:26:34 +0700

    checkout and webhook

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 
 import { ModalProvider } from '@/providers/modal-provider';
 import { ToastProvider } from '@/providers/toast-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +19,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <ClerkProvider>
          <html lang="en">
             <body className={inter.className}>
-               <ModalProvider />
-               <ToastProvider />
-               {children}
+               <ThemeProvider
+                  attribute="class"
+                  defaultTheme="system"
+                  enableSystem
+                  disableTransitionOnChange>
+                  <ModalProvider />
+                  <ToastProvider />
+                  {children}
+               </ThemeProvider>
             </body>
          </html>
       </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 import prismadb from '@/lib/prismadb';
 import { MainNav } from '@/components/main-nav';
 import StoreSwitcher from '@/components/store-switcher';
+import { ThemeToggle } from './theme-toggle';
 
 const Navbar = async () => {
    const { userId } = auth();
@@ -19,6 +20,7 @@ const Navbar = async () => {
             <StoreSwitcher items={stores} />
             <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
+               <ThemeToggle />
                <UserButton afterSignOutUrl="/" />
             </div>
          </div>
